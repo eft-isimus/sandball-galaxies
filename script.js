fa0c616c2d805e625f9357589508d89c2a92ba63
eft-isimus <ayushgurule9@gmail.com>
2026-05-05 02:07:44 +0530

    Add showTab function to control tab visibility

--- script.js
+++ script.js
@@ -128,3 +128,11 @@ window.addEventListener("scroll", function () {
 
     draw();
 });
+
+// to control the tabs
+function showTab(id) {
+    document.querySelectorAll('.tab-content')
+        .forEach(el => el.style.display = 'none');
+
+    document.getElementById(id).style.display = 'block';
+}
